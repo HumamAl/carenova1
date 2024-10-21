8384b563d6839fb1507e974ba80298ce12c189a9
HumamAl <git@stackblitz.com>
2024-10-20 22:39:50 -0400

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    plugins: [react()],
+    define: {
+      'process.env': env
+    },
+    server: {
+      proxy: {
+        '/.netlify/functions': {
+          target: 'http://localhost:9999',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/\.netlify\/functions/, '')
+        }
+      }
+    }
+  };
+});
